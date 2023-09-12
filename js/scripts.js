75c8df586fa3b72332dfaf17cdad01f109c7cc08
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2023-09-12 03:19:16 -0300

    función setDigito  - h2 a input en dígito

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // capturar el digto del diplay
 const digitoDisplay = document.getElementById("digito-display");
-console.log(digitoDisplay);
+console.log(digitoDisplay.value);
 // escucho el teclado, si es número lo ejecuto en la calculadora
 document.addEventListener("keydown", (e) =>
   /\d/.test(e.key) ? setDigito(e.key) : console.log(e.key)
@@ -8,13 +8,11 @@ document.addEventListener("keydown", (e) =>
 // función que maneja el dígito arriba
 const setDigito = function (key) {
   console.log("key recibido = " + key);
-  digitoDisplay.innerText = key;
+  digitoDisplay.value =
+    digitoDisplay.value == 0 ? key : digitoDisplay.value + key;
 };
 
 // capturo todos los botones
 const botones = document.getElementsByClassName("calculadora-tecla");
 // console.log(botones);
 //recorro los botones y le agrego un event listener
-Object.values(botones).forEach((element) => {
-  // console.log(element);
-});
